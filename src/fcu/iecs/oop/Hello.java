07d4e2b0e36d3baf7b0bc117696c528a3d4fcc02
user <user@FG07>
2016-10-07 09:21:40 +0800

    hw2

---
+++ src/fcu/iecs/oop/Hello.java
@@ -0,0 +1,29 @@
+package fcu.iecs.oop;
+import java.util.Scanner;
+public class Hello {
+
+	public static void main(String[] args) {
+		int num1;
+		float num2,num3;
+		String name;
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Enter a integer:");
+		num1 = scanner.nextInt();
+		
+		System.out.println("Enter a float point number:");
+		num2 = scanner.nextFloat();
+		
+		System.out.println("Enter a you name:");
+		name = scanner.next();
+		//name = scanner.nextLine();
+		
+		num3=num1*num2;
+		
+		System.out.println("Hi "+name+ ", the multiplication of "+num1+" and "+num2+" is "+num3);
+		
+		
+	}
+
+}
